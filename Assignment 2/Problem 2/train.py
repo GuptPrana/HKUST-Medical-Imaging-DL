ae6f868f605d54454ce3fd97a9d24acc3d988727
Pranav Gupta <68948549+GuptPrana@users.noreply.github.com>
2022-03-26 18:37:57 +0800

    Progress
    Left: CB/NCE Loss, MCR
    Try

--- Assignment 2/Problem 2/train.py
+++ Assignment 2/Problem 2/train.py
@@ -15,11 +15,10 @@ import numpy as np
 from sklearn.metrics import accuracy_score
 from tqdm import tqdm
 
-from utils.metric import mean_class_recall
+# from utils.metric import mean_class_recall
 
 import dataset
-
-from losses import NCELoss
+# from losses import NCELoss
 # from loss import class_balanced_loss
 
 
@@ -38,8 +37,10 @@ test_transform = None
 trainset = dataset.Skin7(train=True, transform=train_transform)
 testset = dataset.Skin7(train=False, transform=test_transform)
 
+# Iterable loader
 batch_size = 24
 num_workers = 4
+# Convert PIL to tensor?
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True, pin_memory=True,
                                           num_workers=num_workers)
@@ -47,39 +48,84 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                         shuffle=False, pin_memory=True,
                                         num_workers=num_workers)
 
+# Resnet50
+model = models.resnet50(pretrained=True)
+model.fc = nn.Linear(model.fc.in_features, 7)
+model = model.to(device)
+
 # Loss Functions
 # nce = NCELoss().to(device)
 criterion = nn.CrossEntropyLoss().to(device)
 
-# Resnet50 (pass pretrained=True)
-model = models.resnet50()
-
-# Optmizer; Learning Rate
+# Optmizer
+# Learning Rate Scheduler - Can set amsgrad=True
 learn_rate = 1e-3
-optmizer = optim.Adam(model.parameters(), lr=learn_rate)
+optimizer = optim.Adam(model.parameters(), lr=learn_rate)
+
+# Logging all information, log_dir=
+writer = SummaryWriter() #close after running 
 
 # Train 
 Max_epoch = 50
-def train(model,trainloader):
-    # load = iter(trainloader)
-    model = model.to(device)
-    model.train()
-
+def train(model, trainloader, testloader):
+    # Epoch
     for epoch in range(1, Max_epoch+1):
-        
-       
-        for batch_idx, ([data,data_aug], target) in tqdm(enumerate(trainloader)):
-            pass
-   
-def test(model, testloader, epoch):
-    model.eval()
+        print(f'Epoch {epoch}/{Max_epoch}')
+        print('-' * 10)
 
+        model.train()
+        running_loss = 0.0
+        running_correct = 0
+
+        # for batch_idx, ([data,data_aug], target) in tqdm(enumerate(trainloader)):
+        for batch_idx, (data, target) in tqdm(enumerate(trainloader)):
+            # Train
+            optimizer.zero_grad() 
+            images, labels = data, target
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            # Step
+            loss = criterion(outputs, labels)
+            # nce loss =
+            loss.backward()
+            optimizer.step()
+
+            pred = torch.argmax(outputs, dim=1)
+            running_loss += loss.item()
+            running_correct += torch.sum(pred == labels)
+
+        # Save data for plotting curves
+        train_loss = running_loss/len(trainset)
+        train_acc = running_correct/len(trainset)
+        # Can add mean class recall
+        writer.add_scalar("Loss/Train", train_loss, epoch)
+        writer.add_scalar("Acc/Train", train_acc, epoch)
+        print("=> Epoch:{} - train acc: {:.4f}".format(epoch, train_acc))
+
+        # Run Test for this Epoch
+        test(model, testloader, epoch) 
+
+
+def test(model, testloader, epoch):
+    # test mode
+    model.eval()
     y_true = []
     y_pred = []
-    for _, ([data,data_aug], target) in enumerate(testloader):
-       pass
-
-    acc = accuracy_score(y_true, y_pred)
-    print("=> Epoch:{} - val acc: {:.4f}".format(epoch, acc))
+    # for _, ([data,data_aug], target) in enumerate(testloader):
+    for _, (data, target) in enumerate(testloader):
+        data = data.to(device)
+        target = target.to(device)
+        output = model(data)
+        pred = torch.argmax(output, dim=1)
+        # .extend
+        y_pred.extend(pred)
+        y_true.extend(target)
+    
+    test_acc = accuracy_score(y_true, y_pred)
+    # Mean class recall
+    writer.add_scalar("Acc/Test", test_acc, epoch)
+    # writer.add_scalar("MCR/Test", , epoch)
+    print("=> Epoch:{} - val acc: {:.4f}".format(epoch, test_acc))
     
     
